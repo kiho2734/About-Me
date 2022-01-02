7676a0ec088bab0a7de7f3c69099f0bceda0b1ec
kiho <kiho2735@gmail.com>
2022-01-02 13:42:30 -0500

    Close menu tab when clicking nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ const Navbar = () => {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   return (
     <>
@@ -17,28 +18,36 @@ const Navbar = () => {
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              <Link to="/" className="nav-link">
+              <Link to="/" className="nav-link" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/about" className="nav-link">
+              <Link to="/about" className="nav-link" onClick={closeMobileMenu}>
                 About Me
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/skills" className="nav-link">
+              <Link to="/skills" className="nav-link" onClick={closeMobileMenu}>
                 Skills
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/projects" className="nav-link">
+              <Link
+                to="/projects"
+                className="nav-link"
+                onClick={closeMobileMenu}
+              >
                 Projects
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/contact" className="nav-link">
-                Contact Me
+              <Link
+                to="/contact"
+                className="nav-link"
+                onClick={closeMobileMenu}
+              >
+                Contact
               </Link>
             </li>
           </ul>
